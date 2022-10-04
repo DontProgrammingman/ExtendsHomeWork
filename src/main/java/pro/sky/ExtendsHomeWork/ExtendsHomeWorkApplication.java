df99ac1d3e2c4568a8680b73ff8192e5a7f70e7e
serge <clonsfer@gmail.com>
2022-10-04 10:53:48 +0300

    initial commit

---
+++ src/main/java/pro/sky/ExtendsHomeWork/ExtendsHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.ExtendsHomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendsHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendsHomeWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/ExtendsHomeWork/ExtendsHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.ExtendsHomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtendsHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
